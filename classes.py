be9246a00fdfa26de103f6d53a5fab7bb96da730
michzyman <57512259+michyzman@users.noreply.github.com>
2020-07-08 21:03:00 -0400

    Add files via upload

--- classes.py
+++ classes.py
@@ -2,18 +2,26 @@
 import requests
 import csv
 import json 
-
+import dropbox
 
 
 #authToken from Muvi
-api_key = "60abec67a4f2e82147a835ccfb99f792"
+api_key = "MUVIKEY"
 #Query Parameter
-payload = {"authToken": '60abec67a4f2e82147a835ccfb99f792', "permalink": 'classes' , "limit": '1000', "lang_code": 'es',"orderby": 'lastupload' }
+payload = {"authToken": 'MUVIKEY', "permalink": 'classes' , "limit": '1000', "lang_code": 'es',"orderby": 'lastupload' }
 #Open the API and get the information 
 data = requests.get("https://cms.muvi.com/rest/getContentList", params = payload).json()
 
 #Write a new .csv file named contentList
-c = csv.writer(open("contentlist.csv", "w"), lineterminator = '\n')
+c = csv.writer(open("contentList.csv", "w"), lineterminator = '\n')
 #Only write the name of the class and the trainer in the file for each row 
 for item in data ['movieList']:
     c.writerow([item['name'], item ['custom6']])
+
+#drop
+dbx = dropbox.Dropbox ('DROPBOXKEY')
+file_from = '/Users/michellezyman/Projects/Club247/contentlist.csv'
+file_to = '/Club247/contentList.csv'
+with open("contentlist.csv", 'rb') as f:
+    csv_reader = csv.reader(f)
+    dbx.files_upload(f.read(), file_to)
