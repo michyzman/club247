141b4971db8f201653d4f2694347a9d0d49cedaf
Carlos <cjfp29894@gmail.com>
2020-07-09 12:57:19 -0400

    Refactor all code to work with dropbox

--- classes.py
+++ classes.py
@@ -3,25 +3,40 @@ import requests
 import csv
 import json 
 import dropbox
+import pandas as pd
 
+# Tokens
+muvi_api_key = "MUVIKEY"
+dropbox_api_token = "DROPBOXTOKEN"
 
-#authToken from Muvi
-api_key = "MUVIKEY"
-#Query Parameter
-payload = {"authToken": 'MUVIKEY', "permalink": 'classes' , "limit": '1000', "lang_code": 'es',"orderby": 'lastupload' }
+# Query Parameter payload
+payload = {
+        "authToken": muvi_api_key, 
+        "permalink": 'classes' , 
+        "limit": '1000', 
+        "lang_code": 'es',
+        "orderby": 'lastupload'
+        }
+
 #Open the API and get the information 
-data = requests.get("https://cms.muvi.com/rest/getContentList", params = payload).json()
+data = requests.get(
+            "https://cms.muvi.com/rest/getContentList", 
+            params = payload
+        ).json()
 
-#Write a new .csv file named contentList
-c = csv.writer(open("contentList.csv", "w"), lineterminator = '\n')
-#Only write the name of the class and the trainer in the file for each row 
-for item in data ['movieList']:
-    c.writerow([item['name'], item ['custom6']])
+df = pd.DataFrame(data.get('movieList'))
+df = df[['name', 'custom6']]
 
-#drop
-dbx = dropbox.Dropbox ('DROPBOXKEY')
 file_from = '/Users/michellezyman/Projects/Club247/contentlist.csv'
 file_to = '/Club247/contentList.csv'
-with open("contentlist.csv", 'rb') as f:
-    csv_reader = csv.reader(f)
-    dbx.files_upload(f.read(), file_to)
+
+#drop
+conn = dropbox.Dropbox(dropbox_api_token)
+
+df_bytes = bytes(df.to_csv(index=False), 'utf8')
+
+conn.files_upload(
+    f=df_bytes,
+    path=file_to,
+    mode=dropbox.files.WriteMode.overwrite
+)
